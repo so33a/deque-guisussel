962ef83bf352365cc254693090032891cb3209cd
Guilherme Sussel <guisussel@gmail.com>
2016-09-14 20:40:05 -0300

    Atualizado

--- fila.h
+++ fila.h
@@ -5,6 +5,7 @@ typedef struct node * link;
 struct node {
   int item;
   link next;
+  link prev;
 };
 
 typedef struct {
@@ -12,12 +13,16 @@ typedef struct {
   link maisNovo;
 } * FILA;
 
-link novoNo(int item, link next);
+link novoNo(int item, link next, link prev);
 FILA novaFila();
-void inserir(FILA f, int e);
-int remover(FILA f);
-void imprimirFila(FILA f);
+void inserirInicio(FILA f, int e);
+void inserirFim(FILA f, int e);
+int removerFim(FILA f);
+int removerInicio(FILA f);
+void imprimirFilaDireitaParaEsquerda(FILA f);
+void imprimirFilaEsquerdaParaDireita(FILA f);
 void destroiFila(FILA f);
-int filaVazia(FILA f); 
+int filaVazia(FILA f);
+int removerInicio(FILA f);
 
-#endif 
+#endif
